f44f0610e4ed4f32bbb12269b7f41a2d03d4e2f2
Gabe Ambrosio <lu.longshotgaming@gmail.com>
2019-09-01 17:15:14 -0500

    Created Coming soon page and removed static files

--- LongshotGaming/settings.py
+++ LongshotGaming/settings.py
@@ -129,6 +129,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'ComingSoon', 'templates'),
+    os.path.join(PROJECT_ROOT, 'assets'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
