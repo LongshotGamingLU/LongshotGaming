248d65bdb0090ee8a1564fef6902c5da29759870
Gabe Ambrosio <lu.longshotgaming@gmail.com>
2020-02-13 18:57:13 -0600

    Update adding missing files

---
+++ Homepage/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {}
+    return render(request, "Homepage/index.html", context)
