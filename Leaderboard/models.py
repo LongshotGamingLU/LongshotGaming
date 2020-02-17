e21dfe8a482d2d11b9299cce7844d40f4af9f68a
Gabe Ambrosio <lu.longshotgaming@gmail.com>
2020-02-17 11:19:55 -0600

    Add User and Rank table

--- Leaderboard/models.py
+++ Leaderboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Rank(models.Model):
+    userId = models.ForeignKey(
+        'User.User',
+        on_delete=models.CASCADE,
+    )
+    rank = models.IntegerField()
+    gamesWon = models.IntegerField()
+    gamesLost = models.IntegerField()
