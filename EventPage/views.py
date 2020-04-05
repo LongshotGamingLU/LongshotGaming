cff1751d0b111d324588ea2256caae0680e2ddab
Gabe Ambrosio <lu.longshotgaming@gmail.com>
2020-04-05 12:13:44 -0500

    Push

--- EventPage/views.py
+++ EventPage/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 # Google Calendar imports
-import datetime
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+# import datetime
+# import pickle
+# import os.path
+# from googleapiclient.discovery import build
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
