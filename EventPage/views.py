014bf2a7762747132d70aa8f6948994d0b38f730
Gabe Ambrosio <lu.longshotgaming@gmail.com>
2020-04-05 13:32:46 -0500

    Update with Google Calendar integration

--- EventPage/views.py
+++ EventPage/views.py
@@ -1,48 +1,61 @@
 from django.shortcuts import render
 
 # Google Calendar imports
-# import datetime
-# import pickle
-# import os.path
-# from googleapiclient.discovery import build
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from google.auth.transport.requests import Request
+import datetime
+from google.oauth2 import service_account
+import googleapiclient.discovery
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+SERVICE_ACCOUNT_FILE = 'google-credentials.json'
+CAL_ID = 'lu.longshotgaming@gmail.com'
 
-# def importCalendar():
-#     creds = None
+def lastDayOfMonth(any_day):
+    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)  # this will never fail
+    return next_month - datetime.timedelta(days=next_month.day)
 
-#     if os.path.exists('token.pickle'):
-#         with open('token.pickle', 'rb') as token:
-#             creds = pickle.load(token)
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             f = open('credentials.json', 'w')
-#             f.write(os.environ.get('GOOGLE_CRED_FILE'))
-#             f.close()
-#             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-#             creds = flow.run_local_server(port=0)
-#         with open('token.pickle', 'wb') as token:
-#             pickle.dump(creds, token)
-#     service = build('calendar', 'v3', credentials=creds)
+# Imports calendar from lu.longshotgaming@gmail.com for current month
+def importCalendar():
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
 
-#     now = datetime.datetime.utcnow().isoformat() + 'Z'
-#     print("Getting the upcoming 10 events")
-#     events_result = service.events().list(calendarId='primary', timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
-#     events = events_result.get('items', [])
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    monthBegin = datetime.datetime.today().replace(day=1)
+    monthEnd = lastDayOfMonth(datetime.datetime.today())
+
+    events_result = service.events().list(calendarId=CAL_ID,timeMin=(monthBegin.isoformat() + 'Z'),timeMax=(monthEnd.isoformat() + 'Z')).execute()
+
+    events = events_result.get('items', [])
+
+    # PrettyEvents will contain a list of events that are tuple objects.
+    # Each object will be formatted as:
+    # (date, summary) where the format of the objects are:
+    #    Date:    String Format "YYYY-MM-DD", which is, Year-Month-Day
+    #    Summary: String
+    prettyEvents = {
+        'events': []
+    }
+    for e in events:
+        if (e.get('summary') != None):
+            if (e['start'].get('date') != None):
+                prettyEvents['events'].append( ([(e['start']['date'])], e['summary']) )
+            elif (e['start'].get('dateTime') != None):
+                dateString = e['start']['dateTime'][:10]
+                try:
+                    dateTime = datetime.datetime.strptime(dateString, '%Y-%m-%d')
+                    prettyEvents['events'].append( ([dateTime.date().strftime("%Y-%m-%d")], e['summary']) )
+                except ValueError as e:
+                    print("Unable to parse calendar event: " + e['summary'] + ". Skipping event.")
+            else:
+                print("Unknown date for event: " + e['summary'] + ". Skipping event.")
+        else:
+            print("Uknown summary for event. Skipping event.")
     
-#     if not events:
-#         print("No upcoming events found.")
-#     else:
-#         start = event['start'].get('dateTime', event['start'].get('date'))
-#         print(start, event['summary'])
+    return prettyEvents
+            
 
 # Create your views here.
 def index(request):
     context = {}
-    importCalendar()
-    return render(request, "EventPage/index.html", context)
+    prettyEvents = importCalendar()
+    print(prettyEvents['events'])
+    return render(request, "EventPage/index.html", prettyEvents)
